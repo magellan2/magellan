192852ceb24fec13539ef76d7fbe52bc81d2d27e
ukuester <ukuester@1c5abfc5-b992-4a1f-8ccd-9ce068e22774>
2004-01-21 23:00:15 +0000

    Adding SkillChartPanel to Magellan core

---
+++ src/java/com/eressea/skillchart/SkillChartJFreeChartPanel.java
@@ -0,0 +1,53 @@
+package com.eressea.skillchart;
+
+import com.jrefinery.chart.*;
+import com.jrefinery.chart.event.*;
+
+import java.util.*;
+import javax.swing.*;
+
+import java.awt.*;
+import java.awt.geom.*;
+import java.awt.event.*;
+
+/**
+ * @author Ulrich Küster
+ * extended for use with tooltips
+ */
+
+public class SkillChartJFreeChartPanel extends JFreeChartPanel {
+
+	// the SkillChartPanel object this JFreeChartPanel is inside
+	private SkillChartPanel skillChartPanel;
+
+  /**
+   * Full constructor: returns a panel containing the specified chart.
+   * @param chart The chart to display in the panel;
+   */
+  public SkillChartJFreeChartPanel(JFreeChart chart, SkillChartPanel skillChartPanel) {
+	super(chart);
+	this.chart = chart;
+	this.skillChartPanel = skillChartPanel;
+	this.chart.addChangeListener(this);
+	setPreferredSize(new Dimension(480, 320));
+	ToolTipManager.sharedInstance().registerComponent(this);
+  }
+
+	public String getToolTipText(MouseEvent e) {
+		Vector barAreas = ((VerticalBarPlot)chart.getPlot()).getBarAreas();
+		if (barAreas.size() == 0) {
+			return "";
+		} else {
+			Point p = e.getPoint();
+			int i = barAreas.size() - 1;
+			for (Iterator iter = barAreas.iterator(); iter.hasNext(); i--) {
+				Rectangle2D rec = (Rectangle2D)iter.next();
+				if (rec.contains(p)) {
+					return skillChartPanel.getToolTip(i);
+				}
+			}
+			return "";
+		}
+	}
+
+}
