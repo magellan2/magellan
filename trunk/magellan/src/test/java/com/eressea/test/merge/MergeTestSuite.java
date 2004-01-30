a925e5b8a7ded50844940a7e704400f68d56da19
pavkovic <pavkovic@1c5abfc5-b992-4a1f-8ccd-9ce068e22774>
2004-01-30 11:20:59 +0000

    changed some tests for merging GameData objects

--- trunk/magellan/src/test/java/com/eressea/test/merge/MergeTestSuite.java
+++ trunk/magellan/src/test/java/com/eressea/test/merge/MergeTestSuite.java
@@ -4,59 +4,12 @@ import junit.framework.TestSuite;
 
 public class MergeTestSuite {
 
-	public static void main(String [] args) {
-		if ((args.length > 0) && args [0].toUpperCase ().equals ("TEXT")) {
-			junit.textui.TestRunner.run (suite ());
-		} else {
-			junit.swingui.TestRunner.run (MergeTestSuite.class);
-		}
-	}
-	
 	public static TestSuite suite() {
 		TestSuite suite = new TestSuite("Merge Test Suite");
 
 		suite.addTest (new TestSuite (WriteGameData.class));
-		suite.addTest (new TestSuite (MergeSimplestGameData.class));
-
-		/*
-	
-
-import junit.framework.*;
-import junit.extensions.TestSetup;
-
-public class AllTestsOneTimeSetup {
-
-    public static Test suite() {
-
-        TestSuite suite = new TestSuite();
-
-        suite.addTest(SomeTest.suite());
-        suite.addTest(AnotherTest.suite());
-
-        TestSetup wrapper = new TestSetup(suite) {
-
-            protected void setUp() {
-                oneTimeSetUp();
-            }
-
-            protected void tearDown() {
-                oneTimeTearDown();
-            }
-        };
-
-        return wrapper;
-    }
-
-    public static void oneTimeSetUp() {
-        // one-time initialization code
-    }
-
-    public static void oneTimeTearDown() {
-        // one-time cleanup code
-    }
-}
-
-		*/
+		//suite.addTest (new TestSuite (MergeSimplestGameData.class));
+		suite.addTest (new TestSuite (MergeSimpleGameData.class));
 		return suite;
 	}
 }
